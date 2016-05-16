6c4e27067266c65e24d72c6798812438a4a61b90
Cristina Fernández López <cristinafl83@gmail.com>
2016-05-16 17:14:46 -0400

    Update tt_burst_calc_aux.m

--- tt_burst_calc_aux.m
+++ tt_burst_calc_aux.m
@@ -4,30 +4,30 @@
 % belonging,dwell time in quadrant, several graphics, maxim and average
 % velocity in a pulse etc.
 
-function [quadrant,pulse,velocities_pulse]=ttt_burst_calc_aux(VEL_OUT)
+function [quadrant,pulse,velocities_pulse]=tt_burst_calc_aux(VEL_OUT)
    %% QUADRANTS
    %First we have to find the quadrant that each pair of (u'w') belongs and 
    %the percentage of dwell time in each quadrant
    %quad= Contains the quadrant wich belongs each pair of u'w'
    %per= Percentage of dwell time in each quadrant of total
-   [quad,per]=ttt_burst_quadrants(VEL_OUT);
+   [quad,per]=tt_burst_quadrants(VEL_OUT);
 
    %% PULSES
    %Here we find the pulses by quadrants
    %pul= Pulse duration (by steps, 1 step=0.04 seg)
    %ind_pul= Index of beggining pulse in the serie 
    %quad_pul= Quadrant belonging to each pulse
-   [pul,ind_pul,quad_pul]=ttt_burst_pulse(quad);
+   [pul,ind_pul,quad_pul]=tt_burst_pulse(quad);
    %[pul,ind_pul,quad_pul]=burst_pulse_prova(quad);
             
    %% Average Velocities primes in each duration pulse
    %v_avg_pul= Structure of u'average and w'average for each pulse
-   [v_avg_pul]=ttt_burst_vel_avg_pul(VEL_OUT,ind_pul); 
+   [v_avg_pul]=tt_burst_vel_avg_pul(VEL_OUT,ind_pul); 
     
    %% Maximum Velocities primes in each duration pulse
    %v_max_pul= Structure of u'maximum and w'maximum for each pulse (where
    %the norm is maximum in the range)
-   [v_max_pul]=ttt_burst_vel_max_pul(VEL_OUT,ind_pul);
+   [v_max_pul]=tt_burst_vel_max_pul(VEL_OUT,ind_pul);
     
    %% Separating the results (average and maximum velocities) by size pulse
    %v_avg_pul_dur= Structure of average velocities primes by pulse duration
@@ -35,7 +35,7 @@ function [quadrant,pulse,velocities_pulse]=ttt_burst_calc_aux(VEL_OUT)
    %v_avg_mean_pul_dur = Vector of mean velocity in each pulse duration
    %v_max_mean_pul_dur = Vector of maximum velocity in each pulse duration
    %percent_dur_pul= Percentage of total time of each pulse duration
-   [v_avg_pul_dur,v_avg_mean_pul_dur,v_max_pul_dur,v_max_mean_pul_dur,percent_pul_dur,v_quad_pul_dur]=ttt_burst_pulse_sep(pul,v_avg_pul,v_max_pul,quad_pul);
+   [v_avg_pul_dur,v_avg_mean_pul_dur,v_max_pul_dur,v_max_mean_pul_dur,percent_pul_dur,v_quad_pul_dur]=tt_burst_pulse_sep(pul,v_avg_pul,v_max_pul,quad_pul);
          
     %% Summary (Creating Structures of results)
     %Pulse Results
